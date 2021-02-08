f832a3ad0b6e71bf0acb29c0a08effcc4090bde1
Sirm23333 <sunyuping2018@163.com>
2021-02-08 13:14:08 +0800

    统计类测试

---
+++ java-base/src/modifier/access1/TestSameLevelClasses.java
@@ -0,0 +1,21 @@
+package modifier.access1;
+
+/**
+ * 同级类测试
+ * ABC可以被同包下的其他类访问
+ */
+public class TestSameLevelClasses {
+
+}
+// 无修饰符
+class A{
+
+}
+// abstract 修饰
+abstract class B{
+
+}
+// final修饰
+final  class C{
+
+}
