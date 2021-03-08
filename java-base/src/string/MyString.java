b715279f310287ee54672e8d0f9e31d56dd7421e
Sirm23333 <sunyuping2018@163.com>
2021-03-08 17:03:16 +0800

    String

---
+++ java-base/src/string/MyString.java
@@ -0,0 +1,32 @@
+package string;
+
+/**
+ *
+ */
+public class MyString implements java.io.Serializable, Comparable<String>, CharSequence {
+
+
+    public MyString(){
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    @Override
+    public int compareTo(String o) {
+        return 0;
+    }
+}
---
+++ java-base/src/string/StringTest.java
@@ -0,0 +1,10 @@
+package string;
+
+/**
+ *
+ */
+public class StringTest {
+    String string = new String();
+    StringBuffer stringBuffer = new StringBuffer();
+    StringBuilder stringBuilder = new StringBuilder();
+}
