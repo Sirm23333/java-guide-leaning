4db802423629d2cac46ff4b87d59baabef897d39
sirm <sunyuping2018@163.com>
2021-04-12 17:17:29 +0800

    GoF：简单工厂

--- java-GoF/src/com/gof/static_factory/Main.java
+++ java-GoF/src/com/gof/static_factory/Main.java
@@ -7,7 +7,7 @@ package com.gof.static_factory;
  *      2.不用修改业务代码，只需修改工厂类的代码，就可以达到管理对象，一定程度上解耦
  * 缺点：
  *      1.工厂类职责过重，若出现问题，整个系统瘫痪
- *      2.需要修改工厂类的代码
+ *      2.需要修改工厂类的代码，一定程度上违反开闭原则
  */
 public class Main {
     public static void main(String[] args) {
