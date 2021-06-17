bbe05bb9b9458122546ce8df42fbe4be9044d4a0
Sirm <sunyuping2018@163.com>
2021-06-17 16:40:29 +0800

    HashMap匿名内部类

--- java-base/src/clazz/AnonymousInnerClass.java
+++ java-base/src/clazz/AnonymousInnerClass.java
@@ -7,7 +7,7 @@ public class AnonymousInnerClass {
 
     public static void main(String[] args) {
         // 实际为创建了一个AnonymousInnerClass的匿名内部类，该类继承了HashMap
-        // 改匿名内部类中执行了一段非静态代码块
+        // 该匿名内部类中执行了一段非静态代码块
         // 非静态代码块，编译后会放到构造方法的"前部"，所以提前执行
         Map<String,Integer> map = new HashMap<String,Integer>(){
             int i = 2;
